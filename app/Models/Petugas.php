974bd2a0d4a03338759705a26e544f86941408a6
Your Name <you@example.com>
2026-02-05 07:26:10 +0000

    memperbaiki kebodohan gemini

--- app/Models/Petugas.php
+++ app/Models/Petugas.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Petugas extends Model
 {
@@ -18,4 +19,20 @@ class Petugas extends Model
         'golongan',
         'jabatan',
     ];
+    
+    public function getNipFormattedAttribute()
+    {
+        $nip = $this->nip;
+
+        if (strlen($nip) === 18) {
+            return Str::substr($nip, 0, 8) . ' ' .
+                   Str::substr($nip, 8, 6) . ' ' .
+                   Str::substr($nip, 14, 1) . ' ' .
+                   Str::substr($nip, 15, 3);
+        }
+
+        // Return original NIP if it's not 18 digits
+        return $nip;
+    }
+
 }
