79ababe80f149677673f13452581fc94e83510a2
Your Name <you@example.com>
2026-03-06 10:17:40 +0000

    perbaiki header

--- resources/views/lpt/index.blade.php
+++ resources/views/lpt/index.blade.php
@@ -80,7 +80,7 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">
-                    <strong>Data Laporan Pelaksanaan Tugas (LPT)</strong>
+                    <h5><strong>Data Laporan Pelaksanaan Tugas (LPT)</strong></h5>
                 </div>
                 <div class="card-body">
                     <div class="d-flex justify-content-between align-items-center mb-3">
