2ad1e3d50cf0de10b30b3bfdeb6dcbfb899f554f
Your Name <you@example.com>
2026-03-17 15:47:19 +0000

    perbaikan tombol tambah pemeriksaan badan

--- resources/views/pemeriksaan-badan/index.blade.php
+++ resources/views/pemeriksaan-badan/index.blade.php
@@ -44,7 +44,7 @@
     <div class="card">
         <div class="card-header d-flex justify-content-between align-items-center">
             <h5 class="card-title mb-0"><strong>Data Pemeriksaan Badan</strong></h5>
-            <a href="{{ route('pemeriksaan-badan.create') }}" class="btn btn-primary btn-sm">
+            <a href="{{ route('pemeriksaan-badan.create') }}" class="btn btn-primary">
                 <i class="cil-plus"></i> Tambah Data
             </a>
         </div>
