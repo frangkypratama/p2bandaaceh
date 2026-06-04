faebf54c591f088e44e11bf038b45b2b1d6b7f41
Your Name <you@example.com>
2026-06-04 08:40:38 +0000

    perbaikan tombol kembali data sbp

--- resources/views/preview-sbp.blade.php
+++ resources/views/preview-sbp.blade.php
@@ -89,7 +89,7 @@
 
         {{-- Toolbar Cetak + Kembali --}}
         <div class="pdf-toolbar">
-            <a href="{{ route('sbp.index') }}" class="btn btn-secondary">
+            <a href="{{ url()->previous(fallback: route('sbp.index')) }}" class="btn btn-secondary">
                 <i class="cil-arrow-circle-left"></i> Kembali
             </a>
 
