c3224b193913524f9bbf4f0488d17fcea9d3b6d1
frangkypratama <131755162+frangkypratama@users.noreply.github.com>
2026-08-03 10:17:28 +0000

    penamaan lokasi penindakan untuk bandara

--- resources/views/input-sbp/partials/_detail_penindakan.blade.php
+++ resources/views/input-sbp/partials/_detail_penindakan.blade.php
@@ -9,7 +9,10 @@
                     <label for="lokasi_penindakan" class="form-label">Lokasi Penindakan</label>
                     <div class="input-group">
                         <span class="input-group-text"><i class="cil-location-pin"></i></span>
-                        <input id="lokasi_penindakan" type="text" class="form-control" name="lokasi_penindakan" value="{{ old('lokasi_penindakan') }}" placeholder="Contoh: Bandara Internasional Sultan Iskandar Muda/Toko Jaya" required>
+                        <input id="lokasi_penindakan" type="text" class="form-control" list="lokasiPenindakanOptions" name="lokasi_penindakan" value="{{ old('lokasi_penindakan') }}" placeholder="Contoh: Bandara Internasional Sultan Iskandar Muda/Toko Jaya" required>
+                        <datalist id="lokasiPenindakanOptions">
+                            <option value="Bandara Internasional Sultan Iskandar Muda">
+                        </datalist>
                     </div>
                 </div>
                 <div class="col-md-6">
